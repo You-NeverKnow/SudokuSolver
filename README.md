05454a099304ef15db0a8bac62fa01163cd08a41
Advait <advait123@ymail.com>
2018-08-17 09:59:03 -0400

    Added content to README.md, added comments to solver code, added parsing code to solver

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # SudokuSolver
-A very basic sudoku solver
+A very basic sudoku solver using backtracking.
+
+## Algorithm:
+1. Guess a solution for a square based.
+	- Nine Persistent row, col, and Square sets are maintained that keep track of all  
+		the numbers that are not yet present in that rol, col, Square respectively
+	- Possible solutions for a square are obtained by set intersection of a 
+		square's row, col, and Square set.
+	- Then, a solution is chosen at random.
+
+2. Guess solutions for all squares. Backtrack if a square is still empty,
+	but has no candidates for its solution.
+
+3. A complete solution is found when all squares are filled.
+
+
+## Optimization:
+
+### Choosing most constrained square for each step
+Both, choosing an arbitrary square, or choosing the most constrained square, 
+lead to a correct solution. However, choosing the most constrained square for next 
+iteration works a lot faster. For easy puzzles(there is at least one square on 
+board at all times with only one possible solution), we won't have to 
+backtrack at all, reducing the complexity of the solver from 9^n to n^2. 
+
+By choosing a square with fewer choices (say 3) as opposed to a square 
+with 5 choices is an enormous win, since it multiplies for each position.
+If we have 20 positions to fill, we must enumerate only 3^20 (about 3 million) solutions. 
+A branching factor of 5 at each of the 20 positions will result in over
+27000 times as much work!
--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,21 +1,102 @@
 #------------------------------------------------------------------------------
+def main(stringified_board):
+    """
+    1. Parses stringified board into a 2D list
+    2. Validates the sudoku board
+    3. Solves the puzzle
+    4. Returns the output in flat line string
+    """
+    
+    # Parse board
+    board = unstringify_board(stringified_board)
+
+    # Validate sudoku puzzle
+    if not is_valid_sudoku(board):
+        return "Invalid sudoku puzzle"
+    
+    # Solve sudoku
+    solve_sudoku(board)
+
+    # Stringify output board
+    return "".join([x for row in range(9) for x in board[row]])
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+def unstringify_board(stringified_board):
+    """
+    Converts string form of board to 2d list
+    """
+    board = [[] for x in range(9)]
+
+    for i in range(9):
+        board[i] = list(stringified_board[i:i+9])
+    
+    return board
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+def is_valid_sudoku(board):
+    """
+    Checks if an input sudoku board is valid
+
+    Algorithm:
+    For all non-empty squares on board, if value at that square is a number,
+    check if the that value exists in that square's row, column, 
+    and minor square.
+
+    If it is, return False.
+    """
+    
+    cols = [set() for _ in range(9)]
+    squares = [[set() for _ in range(3)] for x in range(3)]
+
+    for row in range(9):
+        rows = set()
+        for col in range(9):
+            if board[row][col] == ".":
+                continue
+            # Check row
+            if board[row][col] in rows:
+                return False
+            else:
+                rows.add(board[row][col])
+
+            # Check col
+            if board[row][col] in cols[col]:
+                return False
+            else:
+                cols[col].add(board[row][col])
+
+            # Check square 
+            if board[row][col] in squares[row // 3][col // 3]:
+                return False
+            else:
+                squares[row // 3][col // 3].add(board[row][col])
+    
+    return True
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
 def solve(board):
     """
     Takes a 9x9 board and solves it
-
     Empty positions are represented by "."
+
+    Initilizes variables required to solve sudoku puzzle, and solves it.
     """
 
-    # Init
+    # Init: Set()s that keep track of numbers encountered in each row, col 
+    # and big square
     row_candidates = [set(str(x) for x in range(1, 10)) for _ in range(9)]
     col_candidates = [set(str(x) for x in range(1, 10)) for _ in range(9)]
     square_candidates = [
-                            [set(str(x) for x in range(1, 10)) for _ in range(3)] 
+                        [set(str(x) for x in range(1, 10)) for _ in range(3)] 
                         for y in range(3)]
 
+    # Keeps track of squares that still are not filled
     empty_squares = set()
 
-    # Parse board
+    # Parse board state
     for row in range(9):
         for col in range(9):
             if board[row][col].isdigit():
@@ -28,24 +109,32 @@ def solve(board):
     _solve(empty_squares, board, row_candidates, 
                     col_candidates, square_candidates)
     
-    return board
 #------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
 def _solve(empty_squares, board, row_candidates, 
                     col_candidates, square_candidates):
     """
+    Algorithm:
+    1. Choose the square that is most constrained: one that has least options.
+    2. For all its candidate solutions, recursively repeat step 1 for next step.
+
     """
+
+    # Exit condition: All squares filled
     if len(empty_squares) == 0:
         return True
 
+    # Select next square
     row, col, candidates = choose_square(empty_squares, row_candidates, 
                                         col_candidates, square_candidates)
     
+    # For all possible solutions for a square, find solutions for other squares
     for candidate in candidates:
         make_move(board, row, col, empty_squares, candidate, 
                 row_candidates, col_candidates, square_candidates)
 
+        # If a globaal solution is found, return all the way back
         if _solve(empty_squares, board, row_candidates, 
                 col_candidates, square_candidates):
             return True
@@ -60,6 +149,10 @@ def _solve(empty_squares, board, row_candidates,
 def choose_square(empty_squares, 
                             row_candidates, col_candidates, square_candidates):
     """
+    Iterates through all the squares, and returns the most constrained squares.
+    
+    Constrained factor is defined as length of intersection of that 
+    square's row, column and encompassing square's sets
     """
     current_square_choice = None
     current_candidates = None
@@ -83,6 +176,7 @@ def choose_square(empty_squares,
 def make_move(board, row, col, empty_squares, candidate, 
                 row_candidates, col_candidates, square_candidates):
     """
+    Change all variables associated with making a move in the puzzle
     """
     update_sets(candidate, row, col, 
                 row_candidates, col_candidates, square_candidates)
@@ -95,6 +189,7 @@ def make_move(board, row, col, empty_squares, candidate,
 def unmake_move(board, row, col, empty_squares, candidate, 
                 row_candidates, col_candidates, square_candidates):
     """
+    Change back all variables changed while making a move in the puzzle
     """
     unupdate_sets(candidate, row, col,
                  row_candidates, col_candidates, square_candidates)
@@ -129,6 +224,8 @@ def update_sets(ch, row, col,
 def get_candidates(row_no, col_no, 
                         row_candidates, col_candidates, square_candidates):
     """
+    Gets possible candidates for a square with 3-way set intersection of its
+    row, col and encompassing square sets
     """
     candidates = row_candidates[row_no] & col_candidates[col_no] & \
                 square_candidates[row_no // 3][col_no // 3]
